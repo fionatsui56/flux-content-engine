c579db977099440ea16d11618d260a8b516ba819
fionatsui56 <fionatsui56@gmail.com>
2026-04-27 11:00:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.htm');
+});
 
 // Single API endpoint for all Gemini calls
 app.post('/api/generate', async (req, res) => {
